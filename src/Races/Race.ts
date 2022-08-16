9721218b55f5fd05035f379deb099ca2a1bfd807
Joao <joaovictorcgjvcg@gmail.com>
2022-08-16 17:54:11 -0400

    Entregando o requisito 1

---
+++ src/Races/Race.ts
@@ -0,0 +1,11 @@
+abstract class Race {
+  constructor(readonly name: string, readonly dexterity: number) {}
+
+  static createdRacesInstances() {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;

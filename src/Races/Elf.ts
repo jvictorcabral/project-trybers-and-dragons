15fdcfab059c3242f4d6c12f956ce8804491387e
Joao <joaovictorcgjvcg@gmail.com>
2022-08-22 18:50:51 -0400

    Entregando o requisito 2

---
+++ src/Races/Elf.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Elf extends Race {
+  private _maxLifePoints: number;
+  static counter = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 99;
+    Elf.counter += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return this.counter;
+  }
+}
+
+export default Elf;

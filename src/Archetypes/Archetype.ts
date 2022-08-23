0215865ee3573ac35d9c6031e238f74cf2bd09ff
Joao <joaovictorcgjvcg@gmail.com>
2022-08-22 20:16:37 -0400

    Entregando o requisito 7

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -1,9 +1,13 @@
+import { EnergyType } from '../Energy';
+
 abstract class Archetype {
   constructor(
     readonly name: string,
     readonly special = 0,
     readonly cost = 0,
   ) {}
+
+  abstract get energyType(): EnergyType;
 }
 
 export default Archetype;

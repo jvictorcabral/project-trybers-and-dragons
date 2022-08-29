e9094657fc317dcac47558353d613f8a5968a8f6
Joao <joaovictorcgjvcg@gmail.com>
2022-08-29 18:43:19 -0400

    entregando o requisito 9

---
+++ src/Monster.ts
@@ -0,0 +1,31 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number { return this._lifePoints; }
+
+  get strength(): number { return this._strength; }
+
+  public attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this.strength);
+  }
+
+  public receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this.lifePoints;
+
+    if (damage > 0) this._lifePoints -= damage;
+
+    if (this.lifePoints <= 0) this._lifePoints = -1;
+
+    return this.lifePoints;
+  }
+}
+
+export default Monster;
